89b6f61e19cf82a8ebac13c8178bd05ca48c5e18
Matheus Castiglioni <matheushcastiglioni@gmail.com>
2018-04-05 22:31:22 -0300

    First commit, uploading game for Github

---
+++ Assets/Scripts/Plane.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plane : MonoBehaviour {
+
+	private Rigidbody2D physic;
+
+	private void Awake () {
+		this.physic = this.GetComponent<Rigidbody2D> ();
+	}
+	
+	private void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			this.Impulse ();
+		}
+	}
+
+	private void Impulse() {
+		this.physic.AddForce (Vector2.up * 10, ForceMode2D.Impulse);
+	}
+
+}
